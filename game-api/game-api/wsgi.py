6e63bb209aa4d43301bd754fa40910ddddbb706c
jeremy.barnes-braingu.com <jeremy.barnes@braingu.com>
2025-09-10 12:11:09 -0400

    fixing rename of game-api

--- game-api/game-api/wsgi.py
+++ game-api/game-api/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'games-api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'game-api.settings')
 
 application = get_wsgi_application()
